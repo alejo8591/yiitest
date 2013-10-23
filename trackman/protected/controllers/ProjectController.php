a01b128984dfa1905ec9ff0ec74c977a30257497
Alejandro Romero <alejo8591@gmail.com>
2013-10-22 21:15:49 -0500

    update project controller with "adduser" access rule

--- trackman/protected/controllers/ProjectController.php
+++ trackman/protected/controllers/ProjectController.php
@@ -32,8 +32,8 @@ class ProjectController extends Controller
 	{
 		return array(
 			array('allow',  // allow all users to perform 'index' and 'view' actions
-				'actions'=>array('index','view'),
-				'users'=>array('*'),
+				'actions'=>array('index','view', 'adduser'),
+				'users'=>array('@'),
 			),
 			array('allow', // allow authenticated user to perform 'create' and 'update' actions
 				'actions'=>array('create','update'),
@@ -197,5 +197,36 @@ class ProjectController extends Controller
 			echo CActiveForm::validate($model);
 			Yii::app()->end();
 		}
+	}
+
+	public function actionAdduser()
+	{
+		$project = $this->loadModel();
+
+		if(Yii::app()->user->checkAccess('createUser', array('project'=>$project)))
+		{
+			throw new CHttpException(403, 'You are not authorized to perform this action.');
+		}
+		$form = new ProjectUserForm;
+		// collect user input data
+		if(isset($_POST['ProjectUserForm']))
+		{
+			$form->attributes=$_POST['ProjectUserForm'];
+			$form->project = $project;
+			// validate user input and set a successfull flash message if valid
+			if($form->validate())
+			{
+				Yii::app()->user->setFlash('success', $form->username . " has been added to the project");
+				$form = new ProjectUserForm;
+			}
+		}
+		// display the add user form
+		$users = User::model()->findAll();
+		$usernames= array();
+		foreach ($users as $user) {
+			$usernames[]=$user->username;
+		}
+		$form->project = $project;
+		$this->render('adduser', array('model'=>$form, 'username'=>$usernames));
 	}
 }
